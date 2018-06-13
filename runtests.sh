f7a81408aa2adfce6275db2f388994e5268674d2
Artur Juraszek <artur@juraszek.xyz>
2018-06-13 15:18:07 +0200

    Fix yet another import problem

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 python -m flake8
-python -m mypy -p pork
+python -m mypy -p pork --ignore-missing-imports
 python -m pytest -v
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+import sys
+
+sys.path.append('/home/asgavar/pork/pork')
